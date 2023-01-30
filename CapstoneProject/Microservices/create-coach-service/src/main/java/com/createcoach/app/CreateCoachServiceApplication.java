b9fbf2a445012e301d100ae9f844eec378fdf35a
Sohail2107 <119490277+Sohail2107@users.noreply.github.com>
2023-01-30 11:30:22 +0000

    Add files via upload

--- CapstoneProject/Microservices/create-coach-service/src/main/java/com/createcoach/app/CreateCoachServiceApplication.java
+++ CapstoneProject/Microservices/create-coach-service/src/main/java/com/createcoach/app/CreateCoachServiceApplication.java
@@ -2,26 +2,28 @@ package com.createcoach.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CreateCoachServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreateCoachServiceApplication.class, args);
 	}
 
-	@Bean
+	/*@Bean
 	public WebMvcConfigurer corsConfigurer() {
 
 	    return new WebMvcConfigurer() {
 
 	        @Override
 	        public void addCorsMappings(CorsRegistry corsRegistry) {
-	            corsRegistry.addMapping("/api/coach/createCoach").allowedOrigins("http://localhost:3000");
+	            corsRegistry.addMapping("/create-coach-service/api/coach/createCoach").allowedOrigins("http://localhost:3000");
 	        }
 	    };
-	}
+	}*/
 }
--- CapstoneProject/Microservices/create-coach-service/src/main/java/com/createcoach/app/controller/CoachController.java
+++ CapstoneProject/Microservices/create-coach-service/src/main/java/com/createcoach/app/controller/CoachController.java
@@ -3,6 +3,7 @@ package com.createcoach.app.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.createcoach.app.request.CreateCoachRequest;
 import com.createcoach.app.response.CoachResponse;
 import com.createcoach.app.service.CoachService;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/coach")
 public class CoachController {
