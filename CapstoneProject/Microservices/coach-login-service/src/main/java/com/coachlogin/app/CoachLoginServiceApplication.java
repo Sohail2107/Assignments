4c51ca6362ecc1b7edf4549f302ede7c0a19b883
Sohail2107 <119490277+Sohail2107@users.noreply.github.com>
2023-01-30 11:29:35 +0000

    Add files via upload

--- CapstoneProject/Microservices/coach-login-service/src/main/java/com/coachlogin/app/CoachLoginServiceApplication.java
+++ CapstoneProject/Microservices/coach-login-service/src/main/java/com/coachlogin/app/CoachLoginServiceApplication.java
@@ -2,26 +2,28 @@ package com.coachlogin.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CoachLoginServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoachLoginServiceApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-
-	    return new WebMvcConfigurer() {
-
-	        @Override
-	        public void addCorsMappings(CorsRegistry corsRegistry) {
-	            corsRegistry.addMapping("/api/coach/coachlogin/**").allowedOrigins("http://localhost:3000");
-	        }
-	    };
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//
+//	    return new WebMvcConfigurer() {
+//
+//	        @Override
+//	        public void addCorsMappings(CorsRegistry corsRegistry) {
+//	            corsRegistry.addMapping("/coach-login-service/api/coach/coachlogin/**").allowedOrigins("http://localhost:3000");
+//	        }
+//	    };
+//	}
 }
