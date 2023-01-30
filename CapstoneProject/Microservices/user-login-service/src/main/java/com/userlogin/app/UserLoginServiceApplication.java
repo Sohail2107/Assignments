e48e547206f82fdec59eb0e0d4014dcd703e76cd
Sohail2107 <119490277+Sohail2107@users.noreply.github.com>
2023-01-30 11:34:36 +0000

    Add files via upload

--- CapstoneProject/Microservices/user-login-service/src/main/java/com/userlogin/app/UserLoginServiceApplication.java
+++ CapstoneProject/Microservices/user-login-service/src/main/java/com/userlogin/app/UserLoginServiceApplication.java
@@ -2,26 +2,28 @@ package com.userlogin.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserLoginServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserLoginServiceApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-
-	    return new WebMvcConfigurer() {
-
-	        @Override
-	        public void addCorsMappings(CorsRegistry corsRegistry) {
-	            corsRegistry.addMapping("/api/user/userlogin/**").allowedOrigins("http://localhost:3000");
-	        }
-	    };
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//
+//	    return new WebMvcConfigurer() {
+//
+//	        @Override
+//	        public void addCorsMappings(CorsRegistry corsRegistry) {
+//	            corsRegistry.addMapping("/user-login-service/api/user/userlogin/**").allowedOrigins("http://localhost:3000");
+//	        }
+//	    };
+//	}
 }
--- CapstoneProject/Microservices/user-login-service/src/main/java/com/userlogin/app/controller/UserLoginController.java
+++ CapstoneProject/Microservices/user-login-service/src/main/java/com/userlogin/app/controller/UserLoginController.java
@@ -3,6 +3,7 @@ package com.userlogin.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.userlogin.app.response.UserResponse;
 import com.userlogin.app.service.UserService;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/user")
 public class UserLoginController {
